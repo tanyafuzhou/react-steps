bfcac65a5a3b6e80b83fee545713b308a77b6b70
tanyafuzhou <tanyafuzhou@gmail.com>
2017-04-03 09:56:54 +0800

    :smile:

--- build.sh
+++
@@ -1,7 +0,0 @@
-rm -rf docs
-mkdir docs
-cp -R libs docs/libs
-./node_modules/.bin/webpack --config webpack.docs.config.js --progress --colors
-rm -rf build
-mkdir build
-cp -R docs/index.js build/index.js
